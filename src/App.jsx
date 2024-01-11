f392119c285a1d5e1d4e7f808b2e52243adb0b18
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-11 14:36:05 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-
+import { Home } from "./Pages/Home/Home";
+import { Login } from "./Pages/Login/Login";
+import { SingleUser } from "./Pages/SingleUser/SingleUser";
+import { NewUser } from "./Pages/NewUser/NewUser";
+import { ListPage } from "./Pages/ListPage/ListPage";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      admin panel
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<ListPage />} />
+              <Route path=":userId" element={<SingleUser />} />
+              <Route path="new" element={<NewUser />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<ListPage />} />
+              <Route path=":productId" element={<SingleUser />} />
+              <Route path="newProduct" element={<NewUser />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
