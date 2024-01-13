a0931b6e7516d13380cc8abe94db8422c8378251
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-13 13:34:37 +0500

    table list

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Chart } from "../../Components/Chart/Chart"
 import { Featured } from "../../Components/Featured/Featured"
 import { Navbar } from "../../Components/Navbar/Navbar"
 import { Sidebar } from "../../Components/Sidebar/Sidebar"
+import { List } from "../../Components/Table/Table"
 import { Widgets } from "../../Components/Widgets/Widgets"
 import "./home.scss"
 
@@ -20,6 +21,12 @@ export const Home = () => {
                 <div className="charts">
                     <Featured />
                     <Chart />
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">
+                        Latest Transactions
+                    </div>
+                    <List />
                 </div>
             </div>
         </div>
