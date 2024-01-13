16cc4a0f9172a95a4b62b911bbd209ce8e334c1a
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-13 17:49:47 +0500

    datatable user page

--- src/Pages/ListPage/ListPage.jsx
+++ src/Pages/ListPage/ListPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Datatable } from '../../Components/Datatable/Datatable'
+import { Navbar } from '../../Components/Navbar/Navbar'
+import { Sidebar } from '../../Components/Sidebar/Sidebar'
+import './listPage.scss'
 
 export const ListPage = () => {
     return (
-        <div>ListPage</div>
+        <div className='list'>
+            <Sidebar />
+            <div className="listContainer">
+                <Navbar />
+                <Datatable />
+            </div>
+        </div>
     )
 }
